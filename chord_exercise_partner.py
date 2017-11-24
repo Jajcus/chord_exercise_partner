4e6e05e3500020c5554328a5a05103c1a5de86f0
Jacek Konieczny <j.konieczny@eggsoft.pl>
2017-11-24 13:30:08 +0100

    setup.py script added
    For package distribution and installation.

--- chord_exercise_partner.py
+++ chord_exercise_partner.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
 from chord_exercise_partner import ui
 
---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(
+    name="jajcus.chord_exercise_partner",
+    version="0.1",
+    packages=["jajcus.chord_exercise_partner"],
+    package_dir={'jajcus': ''},
+    entry_points={
+        'gui_scripts': [
+            "chord_exercise_partner = jajcus.chord_exercise_partner.ui:main",
+        ]
+    },
+    python_requires=">=3.4",
+    extras_require={
+        'MIDI':  ["python-rtmidi"],
+    },
+    zip_safe=False, # ZIP install breaks namespace handling
+)
+
