df7c06a5f21469c66a50077d10005803febc6b5a
Jacek Konieczny <jajcus@jajcus.net>
2017-11-24 21:15:43 +0100

    count pitches from C not A
    That is more consistent with scientific pitch notation and MIDI note
    numbering.

--- chord_exercise_partner/notes.py
+++ chord_exercise_partner/notes.py
@@ -7,9 +7,9 @@ Basic music theory – note names, scale harmonisation.
 # pylint: disable=bad-whitespace,
 
          # 0    1     2    3    4     5    6     7    8    9    10    11
-S_NOTES = ("A", "A♯", "B", "C", "C♯", "D", "D♯", "E", "F", "F♯", "G", "G♯")
-F_NOTES = ("A", "B♭", "B", "C", "D♭", "D", "E♭", "E", "F", "G♭", "G", "A♭")
-FLATS =   (0,   1,    0,   0,   1,    0,   1,    0,   1,   0,    0,    1)
+S_NOTES = ("C", "C♯", "D", "D♯", "E", "F", "F♯", "G", "G♯", "A", "A♯", "B")
+F_NOTES = ("C", "D♭", "D", "E♭", "E", "F", "G♭", "G", "A♭", "A", "B♭", "B")
+FLATS =   (0,   1,    0,   1,    0,   1,   0,    0,   1,    0,   1,    0)
 NOTES = (S_NOTES, F_NOTES)
 
 # Major scale
