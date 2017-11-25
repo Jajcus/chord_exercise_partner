586f849704faafd280f04af5001e5fc09a0b63ce
Jacek Konieczny <jajcus@jajcus.net>
2017-11-25 16:03:30 +0100

    Python version check added to startup script
    Some people used Python 2.7 by mistake, getting non-obvious errors.

--- chord_exercise_partner.py
+++ chord_exercise_partner.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sys
+
+if sys.version_info < (3, 4):
+    print("Python >= 3.4 required!")
+    sys.exit(1)
 
 from chord_exercise_partner import ui
 
